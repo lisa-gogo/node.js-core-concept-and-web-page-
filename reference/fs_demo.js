fd54496a126814909dd13663b38d880489cf53d5
lisa710junyi <83738368+lisa710junyi@users.noreply.github.com>
2021-12-04 09:25:31 -0500

    Update fs_demo.js

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -8,24 +8,26 @@ const path = require('path')
 // })
 
 
-// create and write to file 
+// Create and write to file
 // fs.writeFile(
-//     path.join(__dirname,'/test','hello.txt'),
-//     'Hello world',
-//     err =>{
-//     if(err) throw err;
-//     console.log('Folder written to  ...');
-// })
+//   path.join(__dirname, '/test', 'hello.txt'),
+//   'Hello World!',
+//   err => {
+//     if (err) throw err;
+//     console.log('File written to...');
 
+//     // File append ( callback)
+//     fs.appendFile(
+//       path.join(__dirname, '/test', 'hello.txt'),
+//       ' I love Node.js',
+//       err => {
+//         if (err) throw err;
+//         console.log('File written to...');
+//       }
+//     );
+//   }
+// );
 
-// fs.appendFile(
-//     path.join(__dirname,'/test','hello.txt'),
-//     'I love Node.js',
-//     err => {
-//         if(err) throw err;
-//         console.log('File written to ...')
-//     }
-// )
 
 // read file 
 fs.readFile(path.join(__dirname,'/test','hello.txt'),'utf8',(err,data)=>{
